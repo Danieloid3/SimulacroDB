c908f3f5df310c570e3a9917a9ae99cc87e771c1
Danieloide <danigq000@gmail.com>
2026-03-01 15:50:19 -0500

    README

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.post('/reset-schema', adminController.resetSchema);
 router.post('/migrate-data', adminController.migrateData);
-router.get('/sync-mongo', adminController.syncMongo);
+router.post('/sync-mongo', adminController.syncMongo);
 router.get('/mongo-history', adminController.getMongoHistories);
 
 export default router;
